270fdb59b9f365ef62b68899b09bdbcfbb68c390
Ihor Dmytriv <dmytriwihor@gmail.com>
2025-06-11 19:51:02 +0200

    Implement LoginUserView for "login/" endpoint with Token

--- user/urls.py
+++ user/urls.py
@@ -1,11 +1,10 @@
-# write your code here
-from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path("login/", LoginUserView.as_view(), name="get_token"),
 ]
 
 app_name = "user"
